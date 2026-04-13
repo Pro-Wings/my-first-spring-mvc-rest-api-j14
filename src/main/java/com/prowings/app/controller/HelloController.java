e1cf515e1fd10f4c680c7d5f189896eded835709
JD <jayprowings@gmail.com>
2026-04-13 12:42:59 +0530

    moved to zero xml and explored ResponseEntity with content negotiation

--- src/main/java/com/prowings/app/controller/HelloController.java
+++ src/main/java/com/prowings/app/controller/HelloController.java
@@ -1,7 +1,14 @@
 package com.prowings.app.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,37 +16,38 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prowings.app.model.Address;
+import com.prowings.app.model.ApiResponse;
 import com.prowings.app.model.Student;
 
 @RestController
 public class HelloController {
-	
-	
+
 	@GetMapping("/hello")
-	public String greet()
-	{
+	public String greet() {
 		return "hello Java Developers!!!";
 	}
-	
-	
-	@GetMapping("/students/{id}")
-	public Student getStudentById(@PathVariable("id") Long id)
-	{
-		
-		System.out.println("... calling DB with id : "+id);
-		
+
+	@GetMapping(value = "/students/{id}", produces = { MediaType.APPLICATION_JSON_VALUE,
+			MediaType.APPLICATION_XML_VALUE })
+	public Student getStudentById(@PathVariable("id") Long id) {
+
+		System.out.println("... calling DB with id : " + id);
+
 		Address address = new Address(411033, "Pune", "India");
-		Student fetchedStudent = new Student(111, 10, "Ram", address, true, List.of("Maths","Physics","Chemistry"));
-		
-		return fetchedStudent; 
-		
+		Student fetchedStudent = new Student(111, 10, "Ram", address, true, List.of("Maths", "Physics", "Chemistry"));
+
+		return fetchedStudent;
+
 	}
-	
-	@PostMapping("/students")
-	public void createStudent(@RequestBody Student student)
-	{
+
+	@PostMapping(value ="/students", consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<ApiResponse> createStudent(@RequestBody Student student) {
 		System.out.println("----inside createStudent()-----");
-		System.out.println("----Received Student -----" +student);
+		System.out.println("----Received Student -----" + student);
+		
+//		return ResponseEntity.status(201).header("abc", "pqr").body(student);
+		ApiResponse<Student> apiRes = new ApiResponse<Student>(200, "Successfully added Student in system!!", student);
+		return ResponseEntity.ok(apiRes); 
 	}
 
 }
